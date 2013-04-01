d5e51f818a9a5cbf77d23527399bbe0c5ab7f519
Yan Pujante <yan@pongasoft.com>
2013-04-01 11:31:26 -1000

    #10: use JAVA_HOME if provided

--- org.linkedin.zookeeper-cli/src/cmdline/resources/bin/zk.sh
+++ org.linkedin.zookeeper-cli/src/cmdline/resources/bin/zk.sh
@@ -2,6 +2,7 @@
 
 #
 # Copyright 2010-2010 LinkedIn, Inc
+# Portions Copyright (c) 2013 Yan Pujante
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -37,5 +38,30 @@ JVM_LOG4J=-Dlog4j.configuration=file:$BASEDIR/conf/log4j.xml
 JVM_DEBUG=
 #JVM_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
-#echo java $JVM_LOG4J $JVM_DEBUG $JVM_CLASSPATH org.linkedin.zookeeper.cli.ClientMain $OPTIONS $@
-java $JVM_LOG4J $JVM_DEBUG $JVM_CLASSPATH org.linkedin.zookeeper.cli.ClientMain $OPTIONS $@
+# set JAVA_HOME (JDK6) & JAVA_CMD
+if [ -z "$JAVA_HOME" ]; then
+  case $(uname -s) in
+    SunOS )
+      JAVA_HOME=/export/apps/jdk/JDK-1_6_0_16/usr/java
+      JAVA_CMD=$JAVA_HOME/bin/$(isainfo -n)/java
+      ;;
+    Darwin)
+      JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+      JAVA_CMD=$JAVA_HOME/bin/java
+      ;;
+    * )
+      if [ -z "$JAVA_HOME" ]
+        then
+          echo "Unknown platform (not SunOS or Darwin)!!! Please set JAVA_HOME manually."
+          exit 1
+        fi
+      ;;
+  esac
+fi
+
+if [ -z "$JAVA_CMD" ]; then
+  JAVA_CMD=$JAVA_HOME/bin/java
+fi
+
+#echo $JAVA_CMD $JVM_LOG4J $JVM_DEBUG $JVM_CLASSPATH org.linkedin.zookeeper.cli.ClientMain $OPTIONS $@
+$JAVA_CMD $JVM_LOG4J $JVM_DEBUG $JVM_CLASSPATH org.linkedin.zookeeper.cli.ClientMain $OPTIONS $@
--- org.linkedin.zookeeper-server/src/cmdline/resources/bin/zkEnv.sh
+++ org.linkedin.zookeeper-server/src/cmdline/resources/bin/zkEnv.sh
@@ -41,21 +41,27 @@ done
 CLASSPATH=$ZOOCFGDIR:$CLASSPATH
 
 # set JAVA_HOME (JDK6) & JAVA_CMD
-case $(uname -s) in
-  SunOS ) 
-    JAVA_HOME=/export/apps/jdk/JDK-1_6_0_16/usr/java
-    JAVA_CMD=$JAVA_HOME/bin/$(isainfo -n)/java
-    ;;
-  Darwin) 
-    JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
-    JAVA_CMD=$JAVA_HOME/bin/java
-    ;;
-  * ) 
-    if [ -z "$JAVA_HOME" ] 
-      then
-        echo "Unknown platform (not SunOS or Darwin)!!! Please set JAVA_HOME manually."
-        exit 1
-      fi
-    JAVA_CMD=$JAVA_HOME/bin/java
-    ;;
-esac
+if [ -z "$JAVA_HOME" ]; then
+  case $(uname -s) in
+    SunOS )
+      JAVA_HOME=/export/apps/jdk/JDK-1_6_0_16/usr/java
+      JAVA_CMD=$JAVA_HOME/bin/$(isainfo -n)/java
+      ;;
+    Darwin)
+      JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+      JAVA_CMD=$JAVA_HOME/bin/java
+      ;;
+    * )
+      if [ -z "$JAVA_HOME" ]
+        then
+          echo "Unknown platform (not SunOS or Darwin)!!! Please set JAVA_HOME manually."
+          exit 1
+        fi
+      ;;
+  esac
+fi
+
+if [ -z "$JAVA_CMD" ]; then
+  JAVA_CMD=$JAVA_HOME/bin/java
+fi
+
